b696214ba2c0507043c42ef723ec3c1d25f68341
Suvi Kankare <suvi.kankare@vincit.fi>
2023-01-31 13:02:58 +0200

    AU-321: add error template directory to settings

--- public/sites/default/settings.php
+++ public/sites/default/settings.php
@@ -50,15 +50,14 @@ $config['siteimprove.settings']['api_key'] = getenv('SITEIMPROVE_API_KEY');
 $settings['matomo_site_id'] = getenv('MATOMO_SITE_ID');
 $settings['siteimprove_id'] = getenv('SITEIMPROVE_ID');
 
-$routes = [];
+$settings['error_page']['template_dir'] = '../error_templates';
+
 // Drupal route(s).
-if ($drupal_routes = getenv('DRUPAL_ROUTES')) {
-  $routes = array_map(fn (string $route) => trim($route), explode(',', $drupal_routes));
-}
+$routes = (getenv('DRUPAL_ROUTES')) ? explode(',', getenv('DRUPAL_ROUTES')) : [];
 $routes[] = 'http://127.0.0.1';
 
 foreach ($routes as $route) {
-  $host = parse_url($route, PHP_URL_HOST);
+  $host = parse_url($route)['host'];
   $trusted_host = str_replace('.', '\.', $host);
   $settings['trusted_host_patterns'][] = '^' . $trusted_host . '$';
 }
