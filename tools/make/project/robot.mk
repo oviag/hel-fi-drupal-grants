887b5d30b91da85ee079d9a9d38d3c6fb7e32215
Kristian Polso <kristian@polso.info>
2023-02-01 10:57:56 +0200

    AU-574: Make robot test command use localhost

--- tools/make/project/robot.mk
+++ tools/make/project/robot.mk
@@ -4,6 +4,7 @@ test-robot: ## Run Robot framework tests in docker container
 			-v $(PWD)/test/logs:/opt/robotframework/reports:Z \
 			-v $(PWD)/test:/opt/robotframework/tests:Z \
 			-e TEST_BASEURL=https://$(DRUPAL_HOSTNAME)/ \
-			--add-host $(DRUPAL_HOSTNAME):172.17.0.1 \
+			--add-host $(DRUPAL_HOSTNAME):127.0.0.1 \
+			--net="host" \
 			-it \
 			ppodgorsek/robot-framework:5.0.0
