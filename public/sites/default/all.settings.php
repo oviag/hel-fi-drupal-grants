4fba528fe4a9efb1ae0f9aa8b2e8117e1d992588
Janne Suominen <janne.suominen@siili.com>
2023-02-15 12:29:34 +0200

    fix: Remove overriding settings.

--- public/sites/default/all.settings.php
+++ public/sites/default/all.settings.php
@@ -11,12 +11,6 @@ if (getenv('APP_ENV') == 'production') {
   $config['openid_connect.client.tunnistamoadmin']['settings']['is_production'] = TRUE;
   $config['openid_connect.client.tunnistamoadmin']['settings']['environment_url'] = 'https://api.hel.fi/sso';
 }
-else {
-  $config['openid_connect.client.tunnistamo']['settings']['environment_url'] = 'https://tunnistamo.test.hel.ninja';
-  $config['openid_connect.client.tunnistamo']['settings']['is_production'] = FALSE;
-  $config['openid_connect.client.tunnistamoadmin']['settings']['is_production'] = FALSE;
-  $config['openid_connect.client.tunnistamoadmin']['settings']['environment_url'] = 'https://tunnistamo.test.hel.ninja';
-}
 
 $config['openid_connect.client.tunnistamo']['settings']['client_id'] = getenv('TUNNISTAMO_CLIENT_ID');
 $config['openid_connect.client.tunnistamo']['settings']['client_secret'] = getenv('TUNNISTAMO_CLIENT_SECRET');
