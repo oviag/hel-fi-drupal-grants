c7b93b710f2ca758269db603e54ffc2789cfa634
Janne Suominen <janne.suominen@siili.com>
2023-04-26 08:26:50 +0300

    AU-948: Refactor data maps to support mandates

--- public/modules/custom/grants_applicant_info/src/TypedData/Definition/ApplicantInfoDefinition.php
+++ public/modules/custom/grants_applicant_info/src/TypedData/Definition/ApplicantInfoDefinition.php
@@ -14,7 +14,13 @@ class ApplicantInfoDefinition extends ComplexDataDefinitionBase {
    * {@inheritdoc}
    */
   public function getPropertyDefinitions(): array {
+
     if (!isset($this->propertyDefinitions)) {
+      /** @var \Drupal\grants_profile\GrantsProfileService $grantsProfileService */
+      $grantsProfileService = \Drupal::service('grants_profile.service');
+      $applicantType = $grantsProfileService->getApplicantType();
+
+
       $info = &$this->propertyDefinitions;
 
       $info['applicantType'] = DataDefinition::create('string')
@@ -25,55 +31,95 @@ class ApplicantInfoDefinition extends ComplexDataDefinitionBase {
           'applicantType',
         ]);
 
-      $info['firstname'] = DataDefinition::create('string')
-        ->setLabel('Nimi')
-        ->setSetting('jsonPath', [
-          'compensation',
-          'applicantInfoArray',
-          'firstname',
-        ]);
-      $info['lastname'] = DataDefinition::create('string')
-        ->setLabel('Nimi')
-        ->setSetting('jsonPath', [
-          'compensation',
-          'applicantInfoArray',
-          'lastname',
-        ]);
-      $info['socialSecurityNumber'] = DataDefinition::create('string')
-        ->setLabel('Nimi')
-        ->setSetting('jsonPath', [
-          'compensation',
-          'applicantInfoArray',
-          'socialSecurityNumber',
-        ]);
-      $info['street'] = DataDefinition::create('string')
-        ->setLabel('Nimi')
-        ->setSetting('jsonPath', [
-          'compensation',
-          'currentAddressInfoArray',
-          'street',
-        ]);
-      $info['city'] = DataDefinition::create('string')
-        ->setLabel('Nimi')
-        ->setSetting('jsonPath', [
-          'compensation',
-          'currentAddressInfoArray',
-          'city',
-        ]);
-      $info['postCode'] = DataDefinition::create('string')
-        ->setLabel('Nimi')
-        ->setSetting('jsonPath', [
-          'compensation',
-          'currentAddressInfoArray',
-          'postCode',
-        ]);
-      $info['country'] = DataDefinition::create('string')
-        ->setLabel('Nimi')
-        ->setSetting('jsonPath', [
-          'compensation',
-          'currentAddressInfoArray',
-          'country',
-        ]);
+      if ($applicantType == 'private_person') {
+        $info['firstname'] = DataDefinition::create('string')
+          ->setLabel('Nimi')
+          ->setSetting('jsonPath', [
+            'compensation',
+            'applicantInfoArray',
+            'firstname',
+          ]);
+        $info['lastname'] = DataDefinition::create('string')
+          ->setLabel('Nimi')
+          ->setSetting('jsonPath', [
+            'compensation',
+            'applicantInfoArray',
+            'lastname',
+          ]);
+        $info['socialSecurityNumber'] = DataDefinition::create('string')
+          ->setLabel('Nimi')
+          ->setSetting('jsonPath', [
+            'compensation',
+            'applicantInfoArray',
+            'socialSecurityNumber',
+          ]);
+
+        $info['street'] = DataDefinition::create('string')
+          ->setLabel('Nimi')
+          ->setSetting('jsonPath', [
+            'compensation',
+            'currentAddressInfoArray',
+            'street',
+          ]);
+        $info['city'] = DataDefinition::create('string')
+          ->setLabel('Nimi')
+          ->setSetting('jsonPath', [
+            'compensation',
+            'currentAddressInfoArray',
+            'city',
+          ]);
+        $info['postCode'] = DataDefinition::create('string')
+          ->setLabel('Nimi')
+          ->setSetting('jsonPath', [
+            'compensation',
+            'currentAddressInfoArray',
+            'postCode',
+          ]);
+        $info['country'] = DataDefinition::create('string')
+          ->setLabel('Nimi')
+          ->setSetting('jsonPath', [
+            'compensation',
+            'currentAddressInfoArray',
+            'country',
+          ]);
+      }
+
+      if ($applicantType == 'registered_community') {
+        $info['companyNumber'] = DataDefinition::create('string')
+          ->setLabel('Rekisterinumero')
+          ->setSetting('jsonPath', [
+            'compensation',
+            'applicantInfoArray',
+            'companyNumber',
+          ]);
+        $info['registrationDate'] = DataDefinition::create('datetime_iso8601')
+          ->setLabel('Rekisteröimispäivä')
+          ->setSetting('jsonPath', [
+            'compensation',
+            'applicantInfoArray',
+            'registrationDate',
+          ]);
+
+        $info['foundingYear'] = DataDefinition::create('string')
+          ->setLabel('Perustamisvuosi')
+          ->setSetting('jsonPath', [
+            'compensation',
+            'applicantInfoArray',
+            'foundingYear',
+          ]);
+        $info['home'] = DataDefinition::create('string')
+          ->setLabel('Kotipaikka')
+          ->setSetting('jsonPath', ['compensation', 'applicantInfoArray', 'home']);
+
+        $info['homePage'] = DataDefinition::create('string')
+          ->setLabel('www-sivut')
+          ->setSetting('jsonPath', [
+            'compensation',
+            'applicantInfoArray',
+            'homePage',
+          ])
+          ->setSetting('defaultValue', "");
+      }
 
       $info['applicantType'] = DataDefinition::create('string')
         ->setLabel('Hakijan tyyppi')
@@ -83,14 +129,6 @@ class ApplicantInfoDefinition extends ComplexDataDefinitionBase {
           'applicantType',
         ])
         ->addConstraint('NotBlank');
-
-      $info['companyNumber'] = DataDefinition::create('string')
-        ->setLabel('Rekisterinumero')
-        ->setSetting('jsonPath', [
-          'compensation',
-          'applicantInfoArray',
-          'companyNumber',
-        ]);
 
       $info['communityOfficialName'] = DataDefinition::create('string')
         ->setLabel('Yhteisön nimi')
@@ -108,33 +146,8 @@ class ApplicantInfoDefinition extends ComplexDataDefinitionBase {
           'applicantInfoArray',
           'communityOfficialNameShort',
         ]);
-      $info['registrationDate'] = DataDefinition::create('datetime_iso8601')
-        ->setLabel('Rekisteröimispäivä')
-        ->setSetting('jsonPath', [
-          'compensation',
-          'applicantInfoArray',
-          'registrationDate',
-        ]);
 
-      $info['foundingYear'] = DataDefinition::create('string')
-        ->setLabel('Perustamisvuosi')
-        ->setSetting('jsonPath', [
-          'compensation',
-          'applicantInfoArray',
-          'foundingYear',
-        ]);
-      $info['home'] = DataDefinition::create('string')
-        ->setLabel('Kotipaikka')
-        ->setSetting('jsonPath', ['compensation', 'applicantInfoArray', 'home']);
 
-      $info['homePage'] = DataDefinition::create('string')
-        ->setLabel('www-sivut')
-        ->setSetting('jsonPath', [
-          'compensation',
-          'applicantInfoArray',
-          'homePage',
-        ])
-        ->setSetting('defaultValue', "");
 
       $info['email'] = DataDefinition::create('email')
         ->setLabel('Sähköpostiosoite')
