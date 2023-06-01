07d4fc7541b6765f4522f61c9a17de52b42c903f
Petri Leinonen <petri.leinonen@siili.com>
2023-06-01 08:39:01 +0300

    fix: AU-1093 date fields no longer have default value (#468)
    * AU-1093: fix: AU-1093 date fields no longer have default value
    
    * AU-1093: fix-drupal
    
    * AU-1093: remove placeholders

--- public/modules/custom/grants_metadata/src/GrantsConverterService.php
+++ public/modules/custom/grants_metadata/src/GrantsConverterService.php
@@ -23,14 +23,18 @@ class GrantsConverterService {
   public function convertDates(string $value, array $arguments): string {
 
     try {
-      $dateObject = new \DateTime($value);
-      if (isset($arguments['dateFormat'])) {
-        $retval = $dateObject->format($arguments['dateFormat']);
+      if ($value === NULL || $value === '' || !isset($value)) {
+        $retval = '';
       }
       else {
-        $retval = $dateObject->format(self::DEFAULT_DATETIME_FORMAT);
+        $dateObject = new \DateTime($value);
+        if (isset($arguments['dateFormat'])) {
+          $retval = $dateObject->format($arguments['dateFormat']);
+        }
+        else {
+          $retval = $dateObject->format(self::DEFAULT_DATETIME_FORMAT);
+        }
       }
-
     }
     catch (\Exception $e) {
       $retval = '';
