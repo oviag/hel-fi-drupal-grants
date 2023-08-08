894a954445f5bdac3692dda66b169de35b0a06a3
suvikankare <kankare.suvi@gmail.com>
2023-08-08 15:34:23 +0300

    feat: AU-1414: create module for automated Preview -link (#617)
    * AU-1414: add preview link module
    * AU-1414: fix errors
    * AU-1414: phpcs
    * AU-1414: refactor allowance
    * AU-1414: fix translations
    * AU-1414: better naming
    * AU-1414: config: form location
    
    ---------
    
    Co-authored-by: Suvi Kankare <suvi.kankare@vincit.fi>
    Co-authored-by: Janne Suominen <janne.suominen@siili.com>

---
+++ public/modules/custom/grants_preview_link/src/Plugin/Block/GrantsPreviewLinkBlock.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\grants_preview_link\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Url;
+
+/**
+ * Provides a grants Preview Link block.
+ *
+ * @Block(
+ *   id = "grants_preview_link",
+ *   admin_label = @Translation("Grants Preview Link"),
+ *   category = @Translation("Oma Asiointi")
+ * )
+ */
+class GrantsPreviewLinkBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   *
+   * @throws \GuzzleHttp\Exception\GuzzleException
+   */
+  public function build() {
+
+    $node = \Drupal::routeMatch()->getParameter('node');
+    $webformArray = $node->get('field_webform')->getValue();
+
+    if ($webformArray) {
+      $webformName = $webformArray[0]['target_id'];
+
+      $link = Url::fromRoute('grants_webform_print.print_webform',
+      [
+        'webform' => $webformName,
+      ]);
+    }
+    else {
+      $link = NULL;
+    }
+
+    $allowanceOptions = ['absolute' => TRUE];
+    $allowanceUrl = Url::fromRoute('entity.node.canonical', ['node' => 43], $allowanceOptions);
+    $allowanceUrl = $allowanceUrl->toString();
+
+    $build = [
+      '#theme' => 'grants_preview_link',
+      '#webformLink' => $link,
+      '#allowanceLink' => $allowanceUrl,
+    ];
+    return $build;
+  }
+
+}
