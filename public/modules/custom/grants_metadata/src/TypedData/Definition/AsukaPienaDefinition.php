b247733f20e388144051629b6d3ed9343e3512ad
ovidiu <ovidiu.ardelean@ibm.com>
2023-08-17 11:30:40 +0300

    PLAENH-962: Asukasosallisuus, pienavustushakemus webform initial commit

---
+++ public/modules/custom/grants_metadata/src/TypedData/Definition/AsukaPienaDefinition.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\grants_metadata\TypedData\Definition;
+
+use Drupal\Core\TypedData\ComplexDataDefinitionBase;
+use Drupal\Core\TypedData\DataDefinition;
+use Drupal\Core\TypedData\ListDataDefinition;
+use Drupal\grants_budget_components\TypedData\Definition\GrantsBudgetInfoDefinition;
+
+/**
+ * Define Asukasosallisuus, pienavustushakemus data.
+ */
+class AsukaPienaDefinition extends ComplexDataDefinitionBase {
+
+  use ApplicationDefinitionTrait;
+
+  /**
+   * Base data definitions for all.
+   *
+   * @return array
+   *   Property definitions.
+   */
+  public function getPropertyDefinitions(): array {
+    if (!isset($this->propertyDefinitions)) {
+
+      $info = &$this->propertyDefinitions;
+
+      foreach ($this->getBaseProperties() as $key => $property) {
+        $info[$key] = $property;
+      }
+
+      // @todo: mappings
+
+
+    }
+
+    return $this->propertyDefinitions;
+  }
+
+  /**
+   * Override property definition.
+   *
+   * @param string $name
+   *   Property name.
+   *
+   * @return \Drupal\Core\TypedData\DataDefinitionInterface|void|null
+   *   Property definition.
+   */
+  public function getPropertyDefinition($name) {
+    $retval = parent::getPropertyDefinition($name);
+    return $retval;
+  }
+
+}
