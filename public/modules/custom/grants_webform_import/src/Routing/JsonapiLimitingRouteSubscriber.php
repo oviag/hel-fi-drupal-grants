9a69ef71e290905a69a461b47f9d3b641e0b7e3b
Paavo Kivistö <paavo.kivisto@brightlyworks.com>
2023-08-31 14:06:15 +0300

    AU-1486 Harden access for json api routes

---
+++ public/modules/custom/grants_webform_import/src/Routing/JsonapiLimitingRouteSubscriber.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\grants_webform_import\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Class JsonapiLimitingRouteSubscriber.
+ *
+ * Allow only users with json_api_user role to access
+ * JSON API routes.
+ */
+class JsonapiLimitingRouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    // Limit access to all jsonapi routes with an extra permission.
+    foreach ($collection as $route) {
+        $defaults = $route->getDefaults();
+        if (!empty($defaults['_is_jsonapi'])) {
+          $route->setRequirement('_role', 'json_api_user');
+        }
+      }
+  }
+}
